faae41f40c0b3b20b5ddb16644c24eda9119eecd
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-18 01:16:28 +0600

    making collection done

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toyCollection = client.db('toyStore').collection('toys');
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
