a784fab4d8a2bb8115b296b7ef028739609ba5f2
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-19 23:44:08 +0600

    Update setup done

--- index.js
+++ index.js
@@ -49,15 +49,23 @@ async function run() {
             res.send(result)
         })
 
-        // delete operaction
-        app.delete('/toys/:id', async (req, res) =>{
+        // delete operation
+        app.delete('/toys/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await toyCollection.deleteOne(query);
-            res.send(query)
+            res.send(result)
         })
-        // delete operaction
+        // delete operation
 
+        // update operation
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.findOne(query);
+            res.send(result)
+        })
+        // update operation
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
