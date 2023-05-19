0b0e34b03447b39a56efb90c48bb4f4a5a54b064
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-19 22:52:32 +0600

    delete setup done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -48,6 +48,16 @@ async function run() {
             const result = await toyCollection.insertOne(addToys);
             res.send(result)
         })
+
+        // delete operaction
+        app.delete('/toys/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toyCollection.deleteOne(query);
+            res.send(query)
+        })
+        // delete operaction
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
